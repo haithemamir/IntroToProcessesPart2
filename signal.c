4c567a7dca4f336dea98ce81f0ecfb46e90b7a68
Haithem Amir <haithem.amir@bison.howard.edu>
2024-10-09 03:22:36 +0000

    Completed part 2

---
+++ signal.c
@@ -0,0 +1,19 @@
+/* hello_signal.c */
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+
+void handler(int signum)
+{ //signal handler
+  printf("Hello World!\n");
+  alarm(5); //exit after printing
+}
+
+int main(int argc, char * argv[])
+{
+  signal(SIGALRM,handler); //register handler to handle SIGALRM
+  alarm(5); //Schedule a SIGALRM for 1 second
+  while(1); //busy wait for signal to be delivered
+  return 0; //never reached
+}
